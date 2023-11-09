0bd3c8ef9b20ad0ff8e93d7bd963fb09ca0a954f
...:::01000101:::... <58969724+SourceLab081@users.noreply.github.com>
2023-11-09 06:39:34 +0000

    update bl-move-high-8b

--- techpack/display/msm/dsi/dsi_panel.c
+++ techpack/display/msm/dsi/dsi_panel.c
@@ -715,11 +715,14 @@ static int dsi_panel_update_backlight(struct dsi_panel *panel,
 	dsi = &panel->mipi_device;
 	bl = &panel->bl_config;
 
+	//bl_lvl = bl_lvl * 9 / 10;
+	if (panel->bl_config.bl_move_high_8b) {
+        	bl_lvl = bl_lvl << 5;
+		bl_lvl = (((bl_lvl & 0xff00)) | ((bl_lvl & 0xe0) >> 4));
+	}
+
 	if (panel->bl_config.bl_inverted_dbv)
 		bl_lvl = (((bl_lvl & 0xff) << 8) | (bl_lvl >> 8));
-
-	if (panel->bl_config.bl_move_high_8b)
-		bl_lvl = bl_lvl >> 8;
 	
 	if (panel->bl_config.bl_dcs_subtype == 0xc2)
 		rc = dsi_panel_dcs_set_display_brightness_c2(dsi, bl_lvl);
